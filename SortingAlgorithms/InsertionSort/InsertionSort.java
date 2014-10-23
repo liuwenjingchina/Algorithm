c1b3476ee76f03fbbf6555980836b5f1c23c1947
liuwenjingchina <liuwenjingchina@gmail.com>
2014-10-23 12:46:37 -0400

    Comments added for insertion sort

--- SortingAlgorithms/InsertionSort/InsertionSort.java
+++ SortingAlgorithms/InsertionSort/InsertionSort.java
@@ -1,4 +1,18 @@
 import java.util.Arrays;
+
+/*
+	Insertion Sort is like sorting a hand of cards.
+	Start from inserting the second card, you start comparing with the last card on
+	your hand, until you find the place to insert in your current card.
+
+	Best Case:
+		O(n)
+	Worse Case:
+		O(n^2)
+	Average Case:
+		O(n^2)
+*/
+
 class InsertionSortApp {
 	public static void main (String[] args){
 		int [] arr = {5,4,2,7,8,9,3,11,10,6};
@@ -12,20 +26,24 @@ class InsertionSortApp {
 	public static void insertionSort(int [] arr){
 		for ( int j = 1; j < arr.length; j ++ )
 		{
-			int key = arr[j];
+			int itemToInsert = arr[j];
 			int i;
 			for ( i = j-1; i >= 0; i -- )
 			{
-				if (arr[i] > key)
+				int itemToCompareWith = arr[i];
+				if (itemToCompareWith > itemToInsert)
 				{
-					arr[i+1] = arr[i];
+					// Shift item to compare with right and continue
+					arr[i+1] = itemToCompareWith;
 				}
 				else
 				{
+					// Found the place for item to insert
 					break;
 				}
-			} 	
-			arr[i+1] = key;
+			} 
+			// Insert the item	
+			arr[i+1] = itemToInsert;
 		}
 	}
 }
