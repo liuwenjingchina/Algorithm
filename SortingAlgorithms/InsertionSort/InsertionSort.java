ecbfc42ca8c2e04ee3ee2e1bb29724c442aae24a
liuwenjingchina <liuwenjingchina@gmail.com>
2014-10-25 22:19:48 -0400

    add new comment to InsertionSort

--- SortingAlgorithms/InsertionSort/InsertionSort.java
+++ SortingAlgorithms/InsertionSort/InsertionSort.java
@@ -1,49 +1,54 @@
 import java.util.Arrays;
 
 /*
-	Insertion Sort is like sorting a hand of cards.
-	Start from inserting the second card, you start comparing with the last card on
-	your hand, until you find the place to insert in your current card.
+    Insertion Sort is like sorting a hand of cards.
+    Start from inserting the second card, you start comparing with the last card on
+    your hand, until you find the place to insert in your current card.
 
-	Best Case:
-		O(n)
-	Worse Case:
-		O(n^2)
-	Average Case:
-		O(n^2)
+    Best Case:
+        O(n)
+    Worse Case:
+        O(n^2)
+    Average Case:
+        O(n^2)
 */
 
 class InsertionSortApp {
-	public static void main (String[] args){
-		int [] arr = {5,4,2,7,8,9,3,11,10,6};
-		System.out.println("The input is:");
-		System.out.println(Arrays.toString(arr));
-		InsertionSortApp.insertionSort(arr);
-		System.out.println("The output is:");
-		System.out.println(Arrays.toString(arr));
-	}
+    public static void main (String[] args){
+        int [] arr = {5,4,2,7,8,9,3,11,10,6};
+        System.out.println("The input is:");
+        System.out.println(Arrays.toString(arr));
+        InsertionSortApp.insertionSort(arr);
+        System.out.println("The output is:");
+        System.out.println(Arrays.toString(arr));
+    }
 
-	public static void insertionSort(int [] arr){
-		for ( int j = 1; j < arr.length; j ++ )
-		{
-			int itemToInsert = arr[j];
-			int i;
-			for ( i = j-1; i >= 0; i -- )
-			{
-				int itemToCompareWith = arr[i];
-				if (itemToCompareWith > itemToInsert)
-				{
-					// Shift item to compare with right and continue
-					arr[i+1] = itemToCompareWith;
-				}
-				else
-				{
-					// Found the place for item to insert
-					break;
-				}
-			} 
-			// Insert the item	
-			arr[i+1] = itemToInsert;
-		}
-	}
+    public static void insertionSort(int [] arr){
+        for ( int j = 1; j < arr.length; j ++ )
+        {
+            // arr[j]: the item to be inserted
+            int itemToInsert = arr[j];
+            int i;
+            for ( i = j-1; i >= 0; i -- )
+            {
+                /*
+                    Compare arr[j] with all items inserted before
+                    until arr[j] is smaller than the item.
+                */
+                int itemToCompareWith = arr[i];
+                if (itemToCompareWith > itemToInsert)
+                {
+                    // Shift item to compare with right and continue
+                    arr[i+1] = itemToCompareWith;
+                }
+                else
+                {
+                    // Found the place for item to insert
+                    break;
+                }
+            } 
+            // Insert the item  
+            arr[i+1] = itemToInsert;
+        }
+    }
 }
