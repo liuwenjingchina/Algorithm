c8f21949b2d7c22ad38674199cb3e41c7bf3b96c
liuwenjingchina <liuwenjingchina@gmail.com>
2014-10-27 18:18:52 -0400

    adding max sub array java files

---
+++ MaxSubarray/maxSubarr.java
@@ -0,0 +1,16 @@
+class maxSubar{
+    public static void main (String args){
+            
+    }
+    
+    public static returnValue find_max_crossing_subarray(A, low, mid, high){
+        
+    }
+
+}
+
+class returnValue{
+    public int maxl;
+    public int maxr;
+    public int maxValue;
+}
